950e8e78768460a264424960c59efc26d8940c19
Terris Kremer <terris.kremer@gmail.com>
2022-12-11 06:44:58 -0500

    add indicator to document title on players turn

--- components/Board/Board.tsx
+++ components/Board/Board.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useMemo } from "react";
+import Head from "next/head";
 import toast from "react-hot-toast";
 import { useGame } from "../../context/GameContext";
 import { useAuth } from "../../context/AuthContext";
@@ -11,19 +12,20 @@ export default function Board() {
   const { user } = useAuth();
   const { game, rollDice, addRollKeep, removeRollKeep, stay } = useGame();
 
+  const isThisPlayersTurn = game?.currentTurn.player === user?.uid;
+
   // Show toast on current users turn
   useEffect(() => {
     if (
       !game ||
       !game.currentTurn.roll?.length ||
-      game.currentTurn?.score >= 0
+      game.currentTurn?.score >= 0 ||
+      !isThisPlayersTurn
     ) {
       return;
     }
-    if (game.currentTurn.player === user?.uid) {
-      toast.success("Your turn!", { duration: 4000 });
-    }
-  }, [game, user?.uid, game?.currentTurn?.roll?.length]);
+    toast.success("Your turn!", { duration: 4000 });
+  }, [game, user?.uid, game?.currentTurn?.roll?.length, isThisPlayersTurn]);
 
   const currentPlayer = game?.players.find(
     (player) => player.uid === user?.uid
@@ -35,73 +37,76 @@ export default function Board() {
       game?.currentTurn.rollKeeps &&
       game?.currentTurn?.rollKeeps?.length > 0);
 
-  console.log("current player", currentPlayer);
-  console.log(game?.currentTurn);
-
   if (!game || !user) return null;
   return (
-    <div className={styles.board}>
-      <p style={{ textAlign: "center" }}>Score: {game.currentTurn.score}</p>
-      <div className={styles["roll-area"]}>
-        {game.currentTurn.roll?.map((value, rollIndex) => (
-          <Die
-            key={`roll-die-${rollIndex}-${value}`}
-            value={value}
-            onClick={() => addRollKeep({ value, rollIndex })}
-            disabled={
-              !canKeepDie({
-                roll: mergeRollAndKeeps(
-                  game.currentTurn.roll,
-                  game.currentTurn.rollKeeps
-                ),
-                die: value,
-              })
-            }
-          />
-        ))}
-      </div>
-      <div className={styles["keep-area"]}>
-        <div className={styles["keep-area-rollkeeps"]}>
-          {game.currentTurn.rollKeeps.map((value, rollKeepsIndex) => (
+    <>
+      <Head>
+        <title>{`${isThisPlayersTurn && "Your Turn! | "} D I C E Y`}</title>
+      </Head>
+
+      <div className={styles.board}>
+        <p style={{ textAlign: "center" }}>Score: {game.currentTurn.score}</p>
+        <div className={styles["roll-area"]}>
+          {game.currentTurn.roll?.map((value, rollIndex) => (
             <Die
-              key={`keep-die-${rollKeepsIndex}-${value}`}
+              key={`roll-die-${rollIndex}-${value}`}
               value={value}
-              onClick={() => removeRollKeep({ value, rollKeepsIndex })}
+              onClick={() => addRollKeep({ value, rollIndex })}
+              disabled={
+                !canKeepDie({
+                  roll: mergeRollAndKeeps(
+                    game.currentTurn.roll,
+                    game.currentTurn.rollKeeps
+                  ),
+                  die: value,
+                })
+              }
             />
           ))}
         </div>
-        <div className={styles["keep-area-turnkeeps"]}>
-          {game.currentTurn.roundKeeps?.map((keepGroup) =>
-            keepGroup.map((val, idx) => (
-              <Die key={`keep-die-${idx}-${val}`} value={val} />
-            ))
-          )}
+        <div className={styles["keep-area"]}>
+          <div className={styles["keep-area-rollkeeps"]}>
+            {game.currentTurn.rollKeeps.map((value, rollKeepsIndex) => (
+              <Die
+                key={`keep-die-${rollKeepsIndex}-${value}`}
+                value={value}
+                onClick={() => removeRollKeep({ value, rollKeepsIndex })}
+              />
+            ))}
+          </div>
+          <div className={styles["keep-area-turnkeeps"]}>
+            {game.currentTurn.roundKeeps?.map((keepGroup) =>
+              keepGroup.map((val, idx) => (
+                <Die key={`keep-die-${idx}-${val}`} value={val} />
+              ))
+            )}
+          </div>
         </div>
-      </div>
 
-      <div style={{ textAlign: "center" }}>
-        <Button
-          title="Roll"
-          onClick={() => rollDice()}
-          disabled={
-            !game.currentTurn.rollComplete ||
-            game.currentTurn.status === "BUSTED"
-          }
-          style={{ marginRight: "2rem" }}
-        />
+        <div style={{ textAlign: "center" }}>
+          <Button
+            title="Roll"
+            onClick={() => rollDice()}
+            disabled={
+              !game.currentTurn.rollComplete ||
+              game.currentTurn.status === "BUSTED"
+            }
+            style={{ marginRight: "2rem" }}
+          />
 
-        <Button
-          title="Stay"
-          onClick={() => stay()}
-          style={{ marginLeft: "2rem" }}
-          disabled={!playerCanStay || game.currentTurn.status === "BUSTED"}
-        />
-      </div>
-      {game.currentTurn.status === "BUSTED" && (
-        <div className={styles.busted}>
-          <h1>You Busted!</h1>
+          <Button
+            title="Stay"
+            onClick={() => stay()}
+            style={{ marginLeft: "2rem" }}
+            disabled={!playerCanStay || game.currentTurn.status === "BUSTED"}
+          />
         </div>
-      )}
-    </div>
+        {game.currentTurn.status === "BUSTED" && (
+          <div className={styles.busted}>
+            <h1>You Busted!</h1>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
