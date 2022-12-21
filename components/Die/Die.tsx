359332072167cb283c2766d6907f781cee5c2818
Terris Kremer <terris.kremer@gmail.com>
2022-12-21 13:58:27 -0500

    add dice dots

--- components/Die/Die.tsx
+++ components/Die/Die.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { getRandomInt } from "../../utils";
 import styles from "./Die.module.scss";
 
@@ -12,14 +13,18 @@ export default function Die({ value, onClick, disabled }: DieProps) {
 
   return (
     <div
-      className={styles.die}
+      className={`${styles.die} ${styles["die-" + value]}`}
       style={{
-        transform: `rotate(${randomPlusOrMinus}0.0${getRandomInt(0, 3)}turn)`,
+        //transform: `rotate(${randomPlusOrMinus}0.0${getRandomInt(0, 3)}turn)`,
         cursor: disabled ? "not-allowed" : "pointer",
       }}
       onClick={() => (!disabled && onClick ? onClick(value) : null)}
     >
-      {value}
+      <>
+        {[...Array(value)].map((v, i) => (
+          <span key={`die-dot-${i}`} className={styles["die-dot"]} />
+        ))}
+      </>
     </div>
   );
 }
